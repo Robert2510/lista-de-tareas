b02094d1c679f9267e4cffe56107717fe3a498c5
Robert2510 <contact.rguzman@gmail.com>
2023-03-15 22:04:30 -0300

    creación de esqueletos error,loading,empty

---
+++ src/componentes/EmptyTodo.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './EmpatyTodo.css';
+
+function EmpatyTodo( ){
+              return (
+                            <div className="EmpatyTodo-container">
+                            <div className="EmpatyTodo">
+                                <p>Crea tu primer tarea</p>
+                            </div>
+                            </div>
+              )
+}
+
+export { EmpatyTodo };
---
+++ src/componentes/TodoLoading.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './TodoLoading.css';
+
+function TodoLoading( ){
+    return (
+       <div className="LoadingTodo-container">
+              <span className="LoadingTodo-completeIcon"></span>
+              <p className="LoadingTodo-text">Cargando tareas...</p>
+              <span className="LoadingTodo-deleteIcon"></span>
+       </div>
+     );
+}
+
+export { TodoLoading };
