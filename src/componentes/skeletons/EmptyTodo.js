8157b97b416fe162380f7bb6302abb93c06026b5
Robert2510 <contact.rguzman@gmail.com>
2023-03-18 00:20:30 -0300

    nueva carpeta skeletons

---
+++ src/componentes/skeletons/EmptyTodo.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './EmpatyTodo.css';
+
+function EmpatyTodo( ){
+              return (
+                            <div className="EmpatyTodo">
+                                <p>Crea tu primer tarea</p>
+                            </div>
+                            
+              )
+}
+
+export { EmpatyTodo };
